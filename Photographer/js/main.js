0a7e29b0fad1bb72fd2398fc4f15c344bb068003
Jane <106870518+Jane-Mix@users.noreply.github.com>
2023-06-30 12:55:24 +0300

    Add files via upload

---
+++ Photographer/js/main.js
@@ -0,0 +1,41 @@
+var swiper = new Swiper(".gallery__swiper", {
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach(function(element) {
+  element.addEventListener('click', open);
+});
+
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
+
+
+  tabItem.forEach(function(item) {
+    item.classList.remove('tabs__btn-item--active');
+  });
+
+  tabTarget.classList.add('tabs__btn-item--active');
+
+  tabContent.forEach(function(item) {
+    item.classList.remove('tabs__content-item--active');
+  });
+
+  document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+};
+
+const menuBtn = document.querySelector('.nav-btn');
+const menu = document.querySelector('.nav__list');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('nav__list--active')
+});
+
+
